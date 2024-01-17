09adf047e2bbb7d55254130713fed95149727ee4
jabet <collegejcastersen@gmail.com>
2024-01-16 19:56:28 -0800

    Update arg name

--- projects/06/assembler.py
+++ projects/06/assembler.py
@@ -161,9 +161,9 @@ def main():
   parser.add_argument('--f', help='File to assemble')
   parser.add_argument('--o', help='Name of output file')
 
-  arg_dict = parser.parse_args()
-  file_path = arg_dict.f
-  output_file = arg_dict.o
+  args = parser.parse_args()
+  file_path = args.f
+  output_file = args.o
 
   assemble(file_path,output_file)
 
